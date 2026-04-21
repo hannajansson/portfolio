95923d5d2a2ecdcc92f0164466f50b76dac7d6d1
Hanna Jansson <hannajansson@Hannas-MacBook-Pro-3.local>
2026-04-21 15:26:19 +0200

    Fixed contact container

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -3,7 +3,15 @@ import '../styles/Connect.css'
 export function Connect() {
   return (
     <section className="connect" id="contact">
-      <h2 className="connect-heading">Let's connect</h2>
+      <a
+        href="https://www.linkedin.com/in/hannaajansson/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="connect-heading"
+        data-cursor="expand"
+      >
+        Let's connect
+      </a>
     </section>
   )
 }
