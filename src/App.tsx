06db8e4d5ee0cf3384fad83a2f7fe9fdc5ecb3a3
Hanna Jansson <hannajansson@Hannas-MacBook-Pro-3.local>
2026-05-15 08:54:38 +0200

    Friday code cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import './styles/Dither.css'
 import { useRouter } from './hooks/useRouter'
 import { useScrollReveal } from './hooks/useScrollReveal'
